7b6e95e4f6aad0a207aa83e2b2a2bcd6c670b11f
Louwrens van Dellen <lthvd@protonmail.com>
2026-06-25 09:02:08 -0600

    add PUBLIC_SIMDB_HOST env var, add , reword comments

--- docker/runtime-config-template.js
+++ docker/runtime-config-template.js
@@ -2,10 +2,10 @@ window.__SIMDB_RUNTIME_CONFIG__ = {
   // Modify runtime settings for the Dockerized dashboard.
   // nginx injects TOKENs at response time.
   servers: [
-    'http://DASHBOARD_HOST:DASHBOARD_PORT/api',
+    'SCHEME://PUBLIC_SIMDB_HOST:DASHBOARD_PORT/api',
   ],
-  defaultServer: 'http://DASHBOARD_HOST:DASHBOARD_PORT/api',
+  defaultServer: 'SCHEME://PUBLIC_SIMDB_HOST:DASHBOARD_PORT/api',
   serverConfig: {
-    'http://DASHBOARD_HOST:DASHBOARD_PORT/api': { requiresAuth: false }
+    'SCHEME://PUBLIC_SIMDB_HOST:DASHBOARD_PORT/api': { requiresAuth: false }
   }
 }
